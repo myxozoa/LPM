0105c52a9ad0b69a814e05ef5b70f3d243efe6d9
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-16 23:26:14 -0700

    Removing travis OSX build for now

--- internals/scripts/ElectronRebuild.js
+++ internals/scripts/ElectronRebuild.js
@@ -9,7 +9,7 @@ const nodeModulesPath = path.join(__dirname, '..', '..', 'app', 'node_modules');
 
 if (Object.keys(dependencies || {}).length > 0 && fs.existsSync(nodeModulesPath)) {
   // eslint-disable-next-line
-  const electronRebuildCmd = '../node_modules/.bin/electron-rebuild --parallel --force --types prod,dev,optional --module-dir .';
+  const electronRebuildCmd = '../node_modules/.bin/electron-rebuild --parallel --types prod,dev,optional --module-dir .';
 
   const cmd = process.platform === 'win32'
     ? electronRebuildCmd.replace(/\//g, '\\')
