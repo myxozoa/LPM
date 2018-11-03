5c53476ad87d680412fc026e04133544d2f6f918
Ronnie Miksch <fakepreferences@gmail.com>
2018-11-02 19:58:47 -0700

    Css reset and beginning layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import '../styles/reset.scss';
 import '../styles/index.scss';
+import '../styles/layout.scss';
 
-console.log('webpack starterkit');
+console.log('%ccreated by %c Ronnie Miksch ', 'font-size: 30px;', 'background: white; color: black; font-size: 40px;');
