8fe2ef5fadab5632e5b9fa9baf0b2e695d21b0ef
Ronnie Miksch <fakepreferences@gmail.com>
2018-11-05 18:14:47 -0800

    Stats image and basic media queries

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import '../styles/reset.scss';
 import '../styles/index.scss';
 import '../styles/layout.scss';
+import '../styles/media_queries.scss';
 
 console.log('%ccreated by %c Ronnie Miksch ', 'font-size: 30px;', 'background: white; color: black; font-size: 40px;');
